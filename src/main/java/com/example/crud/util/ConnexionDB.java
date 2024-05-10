b8db895a862fe3248737a3f346587bc5cb162fb5
unknown <Mohamed Marbouh>
2024-05-10 16:25:07 +0100

    commit

--- src/main/java/com/example/crud/util/ConnexionDB.java
+++ src/main/java/com/example/crud/util/ConnexionDB.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnexionDB {
-    private static final String URL = "jdbc:mysql://dbaas-db-7965936-do-user-15838371-0.c.db.ondigitalocean.com:25060/defaultdb?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "doadmin";
-    private static final String PASSWORD = "AVNS_LBM3vY3Gn2pg6_k8TiR";
+    private static final String URL = "jdbc:mysql://localhost:3306/crud?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static Connection connection;
 
